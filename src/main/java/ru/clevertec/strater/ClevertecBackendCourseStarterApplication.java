8df8dd5dfc199c173422e729b49385e12314d9bd
TratsyakEvgen <tanchik776@gmail.com>
2024-10-16 11:20:18 +0300

    Initial commit

---
+++ src/main/java/ru/clevertec/strater/ClevertecBackendCourseStarterApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.strater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClevertecBackendCourseStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClevertecBackendCourseStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/clevertec/strater/ClevertecBackendCourseStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.strater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClevertecBackendCourseStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
